a10c03ec8588d6e8b7d068607db440833a558aaa
AlazarBekele <alazarthe70@gmail.com>
2024-09-27 00:48:19 +0300

    Create ProfileMode

--- Base/models.py
+++ Base/models.py
@@ -32,3 +32,11 @@ class ContactMessages(models.Model):
 
     def __str__(self) -> str:
         return self.name + " " + self.email
+
+class Profile (models.Model):
+    name = models.CharField(max_length=20)
+    ProImg = models.ImageField (upload_to='photo/')
+    username = models.CharField (max_length=30)
+
+    def __str__(self) -> str:
+        return self.username
