5c9147b27d78355d9a3432b87864d32a394e3d62
AlazarBekele <alazarthe70@gmail.com>
2024-09-27 00:52:09 +0300

    Successfully Deployed profile in Admin

--- Base/admin.py
+++ Base/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import *
+from .models import Category, News, ContactMessages, Profile
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(News)
 admin.site.register(ContactMessages)
+admin.site.register(Profile)
